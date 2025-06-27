9b67f75902e69a9eaf2b021c81cb941cfb4bbed0
Kazik <kazio@kargraf.com.pl>
2025-06-27 18:26:55 +0200

    Preparing master version

--- index.js
+++ index.js
@@ -36,6 +36,7 @@ function calculateIAQ(gasResistance, baseline = 10_000_000) {
     return Math.round(Math.max(0, Math.min(500, iaq)));
 }
 
+console.log('SENSOR EXPORTS:', Object.keys(sensor));
 module.exports = {
     ...sensor,
     calculateIAQ
